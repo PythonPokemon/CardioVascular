4dddd3aa5ef1be0a8986952c3d6e4b6ee65c34ad
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-11-05 10:40:13 +0100

    new Feature: Ethic group, Townsend quitile, do you smoke

--- Cardioaskular/Models/ProfileModel.cs
+++ Cardioaskular/Models/ProfileModel.cs
@@ -283,6 +283,147 @@ namespace CardioVaskular.Models
                 }
             }
         }
+        //----------------------------------------------------------------------------------------->Townsend quintile<
+
+        // String-Eigenschaft zur Speicherung der 'Townsend quintile' Auswahl
+        private string townsendQuintile;
+        public string TownsendQuintile
+        {
+            get { return townsendQuintile; }
+            set
+            {
+                if (townsendQuintile != value)
+                {
+                    townsendQuintile = value;
+                    OnPropertyChanged(nameof(TownsendQuintile));
+                    UpdateTownsendQuintileFlags(); // Methode, um bool-Werte zu setzen
+                }
+            }
+        }
+
+        // Bool-Werte für jede 'Townsend quintile' auswahl
+        public bool IsVeryAffluent { get; private set; }
+        public bool IsAffluent { get; private set; }
+        public bool IsAverage { get; private set; }
+        public bool IsBelowAverage { get; private set; }
+        public bool IsLeastAffluent { get; private set; }
+
+
+        // Methode, um die bool-Werte basierend auf der 'Townsend quintile' Auswahl zu setzen
+        private void UpdateTownsendQuintileFlags()
+        {
+            // Alle bool-Werte auf false setzen
+            IsVeryAffluent = false;
+            IsAffluent = false;
+            IsAverage = false;
+            IsBelowAverage = false;
+            IsLeastAffluent = false;
+            
+
+            // switch-Anweisung zur Auswahl
+            switch (townsendQuintile)
+            {
+                case "Very affluent":
+                    IsVeryAffluent = true;
+                    break;
+                case "Affluent":
+                    IsAffluent = true;
+                    break;
+                case "Average":
+                    IsAverage = true;
+                    break;
+                case "Below average":
+                    IsBelowAverage = true;
+                    break;
+                case "Least affluent":
+                    IsLeastAffluent = true;
+                    break;
+            }
+
+            // Benachrichtigung über Änderungen an den bool-Werten
+            OnPropertyChanged(nameof(IsVeryAffluent));
+            OnPropertyChanged(nameof(IsAffluent));
+            OnPropertyChanged(nameof(IsAverage));
+            OnPropertyChanged(nameof(IsBelowAverage));
+            OnPropertyChanged(nameof(IsLeastAffluent));
+            
+        }
+
+        //----------------------------------------------------------------------------------------->Terms and Conditions<
+        //sind in der code behind datei von 'ProfileView.xaml.cs'
+
+        //----------------------------------------------------------------------------------------->Do you smoke<
+        // String-Eigenschaft zur Speicherung der 'do you smoke' Auswahl
+        private string doYouSmoke;
+        public string DoYouSmoke
+        {
+            get { return doYouSmoke; }
+            set
+            {
+                if (doYouSmoke != value)
+                {
+                    doYouSmoke = value;
+                    OnPropertyChanged(nameof(DoYouSmoke));
+                    UpdateDoYouSmokeFlags(); // Methode, um bool-Werte zu setzen
+                }
+            }
+        }
+
+        // Bool-Werte für jede 'do you smoke' Auswahl
+        public bool IsNo { get; private set; }
+        public bool IsQuit { get; private set; }
+        public bool IsSLT10PerDay { get; private set; }
+        public bool IsSLT20PerDay { get; private set; }
+        public bool IsSmokeOver20PerDay { get; private set; }
+
+
+        // Methode, um die bool-Werte basierend auf der 'do you smoke' Auswahl zu setzen
+        private void UpdateDoYouSmokeFlags()
+        {
+            // Alle bool-Werte auf false setzen
+            IsNo = false;
+            IsQuit = false;
+            IsSLT10PerDay = false;
+            IsSLT20PerDay = false;
+            IsSmokeOver20PerDay = false;
+
+
+            // switch-Anweisung zur Auswahl
+            switch (townsendQuintile)
+            {
+                case "No":
+                    IsNo = true;
+                    break;
+                case "I quit":
+                    IsQuit = true;
+                    break;
+                case "I smoke less than 10/day":
+                    IsSLT10PerDay = true;
+                    break;
+                case "I smoke less than 20/day":
+                    IsSLT20PerDay = true;
+                    break;
+                case "Least affluent":
+                    IsSmokeOver20PerDay = true;
+                    break;
+            }
+
+            // Benachrichtigung über Änderungen an den bool-Werten
+            OnPropertyChanged(nameof(IsNo));
+            OnPropertyChanged(nameof(IsQuit));
+            OnPropertyChanged(nameof(IsSLT10PerDay));
+            OnPropertyChanged(nameof(IsSLT20PerDay));
+            OnPropertyChanged(nameof(IsSmokeOver20PerDay));
+
+        }
+
+        //----------------------------------------------------------------------------------------->Cholesterol & Blood Pressure (mmil/L & mg/dL)<
+
+        //----------------------------------------------------------------------------------------->Lots of CheckBoxes (right side)<
+
+        //----------------------------------------------------------------------------------------->Save / Load / Next<
+        // sind in der code behind datei von 'ProfileView.xaml.cs'
+
         //----------------------------------------------------------------------------------------->EventHandler<
 
         // Event, das ausgelöst wird, wenn sich eine Eigenschaft ändert
